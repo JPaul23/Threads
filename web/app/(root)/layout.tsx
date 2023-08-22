331072aec3dd1a200adc3336d1d5d43c33e39481
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-08-22 22:26:14 +0200

    Adding shadcn UI components

--- web/app/(root)/layout.tsx
+++ web/app/(root)/layout.tsx
@@ -2,7 +2,10 @@ import { ClerkProvider } from '@clerk/nextjs/app-beta';
 import '../globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Topbar, Bottombar, LeftSidebar, RightSidebar } from '../../components';
+import Topbar from '@/components/shared/Topbar';
+import Bottombar from '@/components/shared/Bottombar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
 
 const inter = Inter({ subsets: ['latin'] });
 
