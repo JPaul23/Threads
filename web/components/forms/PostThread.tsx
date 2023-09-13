6bd452225095416b2c68f803939ec4414876263f
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-09-13 22:37:19 +0200

    Create thread & fetch threads

--- web/components/forms/PostThread.tsx
+++ web/components/forms/PostThread.tsx
@@ -24,6 +24,7 @@ import { useUploadThing } from "@/lib/uploadthing";
 
 // import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.action";
 
 interface Props {
   user: {
@@ -44,11 +45,51 @@ function PostThread({ userId }: { userId: string }) {
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
     defaultValues: {
-      thread: '',
-      accountId: userId
+      thread: "",
+      accountId: userId,
     },
   });
-  return <h1>Post Thread form</h1>;
+
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full gap-3">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
 }
 
 export default PostThread;
